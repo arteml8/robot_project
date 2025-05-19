f72d28300f6fcc010dd05978e39d7b7f833aaaa8
Artem Lepilov <artem.lepilov@gmail.com>
2025-05-18 23:08:25 -0400

    adding missing files

---
+++ imu/itg3200_sensor.py
@@ -0,0 +1,18 @@
+from i2c_sensor import I2CSensor
+
+class GyroscopeITG3200(I2CSensor):
+    def __init__(self, bus, address=0x68):
+        super().__init__(bus, address, name = 'ITG3200')
+        self._initialize()
+
+    def _initialize(self):
+        self.write_register(0x3E, 0x00)  # Wake up
+        self.write_register(0x15, 0x07)  # Sample rate divider
+        self.write_register(0x16, 0x18)  # ±2000°/s, 256Hz low-pass
+
+    def read_raw(self):
+        return self.read_raw_data(self.address, 0x1D, 6)
+
+    def read_dps(self):
+        raw = self.read_raw()
+        return tuple(r / 14.375 for r in raw)  # Convert to deg/s
