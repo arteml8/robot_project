8e337e90f03830a66afe816bff01afde87c4348f
Artem Lepilov <artem.lepilov@gmail.com>
2025-05-19 11:21:41 -0400

    typo

--- imu/adxl345_sensor.py
+++ imu/adxl345_sensor.py
@@ -5,7 +5,7 @@ class ADXL345Sensor(I2CSensor):
   def __init__(self, bus_id=1, address=0x53):
     # Call the I2CSensor base constructor
     scale_factor = 256 # default 10 bit signed scale +/-2G 1G @ 256
-    super().__init__(bus_id, address, name="ADXL345", scale factor)
+    super().__init__(bus_id, address, name="ADXL345", scale_factor)
     self._initialize()
 
   def _initialize(self):
