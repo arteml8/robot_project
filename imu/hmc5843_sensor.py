70182dc297bead21701cff59f6c1667487225a32
Artem Lepilov <artem.lepilov@gmail.com>
2025-05-18 22:59:20 -0400

    trying to get raw data from accels first

--- imu/hmc5843_sensor.py
+++ imu/hmc5843_sensor.py
@@ -1,15 +1,17 @@
 from i2c_sensor import I2CSensor
 
 
-class HMC5843(I2CSensor):
-  def __init__(self, address=0x1e, bus_id=1):
+class MagnetometerHMC5843(I2CSensor):
+  def __init__(self, bus_id=1, address=0x1e):
     super().__init__(bus_id, address, name = "HMC5843")
+    self._initialize()
+
+  def _initialize(self):
     self.bus.write_byte_data(self.address, 0x00, 0x70) # Config Register A
     self.bus.write_byte_data(self.address, 0x01, 0xA0) # Config Register B
     self.bus.write_byte_data(self.address, 0x02, 0x00) # Continuous Measurement Mode
   
   def read_raw(self):
-    x = self.read_word(0x03)
-    y = self.read_word(0x05)
-    z = self.read_word(0x07)
-	return self.apply_offsets(x, y, z)
+    return self.read_raw_data(self.address, 0x03)
+
+	# return self.apply_offsets(x, y, z)
--- imu/main.py
+++ imu/main.py
@@ -1,17 +1,28 @@
 from adxl345_sensor import ADXL345Sensor
+from hmc5883l_sensor import MagnetometerHMC5883L
+from itg3200_sensor import GyroscopeITG3200
 from calibration import Calibration
 from plotter import LivePlot2D
 import time
 
-def data_gen(sensor, calib):
-  while True:
-    x, y, z = sensor.read_raw()
-    x, y, z = calib.apply(x, y, z)
-    yield x, y
-    time.sleep(0.1)
+def data_gen(sensor): #, calib):
+	x, y, z = sensor.read_raw()
+	# x, y, z = calib.apply(x, y, z)
+	print(f'{sensor.name}: X: {x}, Y: {y}, Z: {z} \n')
+	time.sleep(0.1)
    
 if __name__ == '__main__':
   adxl = ADXL345Sensor()
-  calib = Calibration(offset=(0,0,0), scale=(1,1,1))
-  plot = LivePlot2D()
-  plot.start(lambda: data_gen(adxl, calib))
+  hmc = MagnetometerHMC5883L()
+  itg = GyroscopeITG3200()
+
+  while True: 
+  	for sensor in (adxl, hmc, itg):
+  		data_gen(sensor)
+  except KeyboardInterrupt:
+  	print("Exited because keyboard")
+
+  
+  # calib = Calibration(offset=(0,0,0), scale=(1,1,1))
+  # plot = LivePlot2D()
+  # plot.start(lambda: data_gen(adxl, calib))
