4c8c262be81880d693787f6c9c229db7704af23b
Artem Lepilov <artem.lepilov@gmail.com>
2025-05-20 11:20:00 -0400

    adding calibration

--- imu/calibration_manager.py
+++ imu/calibration_manager.py
@@ -2,20 +2,18 @@ import json
 import os
 
 class CalibrationManager:
-  def __init__(self, filename='calibration.json'):
+  def __init__(self, sensor_name, filename='calibration.json'):
     self.filename = filename
+    self.sensor_name = sensor_name
     self.calibration_data = {}
-    
-  def load(self):
+
+  def save_offsets(self):
+    calibration = {self.sensor_name : self.calibration_data}
+    with open(self.filename, 'w') as f:
+      json.dump(calibration, f, indent=4)
+
+  def get_offsets(self):
     if os.path.exists(self.filename):
       with open(self.filename, 'r') as f:
         self.calibration_data = json.load(f)
-    return self.calibration_data
-  
-  def save(self, data):
-    self.calibration_data = data
-    with open(self.filename, 'w') as f:
-      json.dump(self.calibration_data, f, indent=4)
-  
-  def get_offsets(self, sensor_name):
-    return self.calibration_data.get(sensor_name, {'x': 0, 'y': 0, 'z': 0})
+    return self.calibration_data.get(self.sensor_name, {'x': 0, 'y': 0, 'z': 0})
