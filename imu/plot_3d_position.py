bb084923771b797f0188f82a682e86625c63f5db
Artem Lepilov <artem.lepilov@gmail.com>
2025-05-19 22:39:52 -0400

    adding more position debug

--- imu/plot_3d_position.py
+++ imu/plot_3d_position.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from imu import IMU
+
 import time
 
 imu = IMU()
@@ -13,6 +14,7 @@ ax = fig.add_subplot(111, projection='3d')
 while True:
     imu.update()
     x, y, z = imu.get_position()
+    print(f"Position: x={x:.4f}, y={y:.4f}, z={z:.4f}")
     positions.append((x, y, z))
 
     if len(positions) > 100:  # sliding window
