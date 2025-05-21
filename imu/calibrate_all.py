33178aef87ab0df670e30cf1d6baf713f5601fd6
Artem Lepilov <artem.lepilov@gmail.com>
2025-05-21 12:50:47 -0400

    fixing sensor imports

--- imu/calibrate_all.py
+++ imu/calibrate_all.py
@@ -1,6 +1,6 @@
-from adxl345 import ADXL345Sensor
-from itg3200 import GyroscopeITG3200
-from hmc5883l import MagnetometerHMC5883L
+from adxl345_sensor import ADXL345Sensor
+from hmc5883l_sensor import MagnetometerHMC5883L
+from itg3200_sensor import GyroscopeITG3200
 
 def main():
     accel = ADXL345Sensor()
