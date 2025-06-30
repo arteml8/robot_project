2f6e1af3bd11f9e0cb18ed174b2a1a480b989c78
Artem Lepilov <artem.lepilov@gmail.com>
2025-06-30 12:52:05 -0400

    adding debug

--- ble_test.py
+++ ble_test.py
@@ -4,9 +4,12 @@ from robot_ble_client import RobotBLEClient
 async def main():
     robot = RobotBLEClient()
     if await robot.connect():
+    	print("CONNECTED!")
         await robot.send("CMD:DRIVE:0.2,0.0,0.0\n")
+        print("Sending go command")
         await asyncio.sleep(3)
         await robot.send("CMD:STOP\n")
+        print("sending stop command")
         await robot.disconnect()
 
 asyncio.run(main())
