51ed96683e5c18e57f3fed80725d7fd7d86620a0
Artem Lepilov <artem.lepilov@gmail.com>
2025-07-18 22:38:34 -0400

    modifying some files

--- ros2_ws/src/robot_nodes/setup.py
+++ ros2_ws/src/robot_nodes/setup.py
@@ -4,7 +4,7 @@ package_name = 'robot_nodes'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -14,12 +14,16 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='artem',
-    maintainer_email='artem.lepilov@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='alepilov@hotmail.com',
     tests_require=['pytest'],
+    description='ROS 2 nodes for robot control of Self Driving House Robot',
+    license='MIT',
     entry_points={
         'console_scripts': [
+            'ble_node = robot_nodes.ble_node:main',
+            'velocity_controller_node = robot_nodes.velocity_controller_node:main',
+            'planner_node = robot_nodes.planner_node:main',
+            'odometry_node = robot_nodes.odometry_node:main'
         ],
     },
 )
