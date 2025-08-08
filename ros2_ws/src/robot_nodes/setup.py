7bd70c99183126c0990812941684a983ddaddf7f
Artem Lepilov <artem.lepilov@gmail.com>
2025-08-08 14:52:11 -0400

    editing setup to finagle packages

--- ros2_ws/src/robot_nodes/setup.py
+++ ros2_ws/src/robot_nodes/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'robot_nodes'
 
 setup(
     name=package_name,
     version='0.1.0',
-    packages=[package_name],
+    packages=find_packages(include=[package_name, package_name + '.*']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
