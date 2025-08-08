64b22872910073353d96d9f13f52406e7a5d6ba8
Artem Lepilov <artem.lepilov@gmail.com>
2025-08-08 16:59:26 -0400

    changing syntax

--- ros2_ws/src/robot_nodes/launch/bringup_launch.py
+++ ros2_ws/src/robot_nodes/launch/bringup_launch.py
@@ -4,9 +4,9 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='robot_nodes', executable='ble_node', name='ble_node', output='screen'),
-        Node(package='robot_nodes', executable='odometry_node', name='odometry', output='screen'),
-        Node(package='robot_nodes', executable='velocity_controller_node', name='velocity_controller', output='screen'),
-        Node(package='robot_nodes', executable='drive_distance_node', name='drive_distance', output='screen'),
-        Node(package='robot_nodes', executable='mock_planner_node', name='mock_planner', output='screen'),
+        Node(package='robot_nodes', node_executable='ble_node', node_name='ble_node', output='screen'),
+        Node(package='robot_nodes', node_executable='odometry_node', node_name='odometry', output='screen'),
+        Node(package='robot_nodes', node_executable='velocity_controller_node', node_name='velocity_controller', output='screen'),
+        Node(package='robot_nodes', node_executable='drive_distance_node', node_name='drive_distance', output='screen'),
+        Node(package='robot_nodes', node_executable='mock_planner_node', node_name='mock_planner', output='screen'),
     ])
